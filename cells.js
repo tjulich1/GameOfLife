77eeece3b40ff86bbddcbeacfc7a4781eec73a38
Trent Julich <tjulich1@gmail.com>
2020-02-27 20:51:39 -0800

    added buttons which initialize the game with different states

--- cells.js
+++ cells.js
@@ -33,11 +33,102 @@ class Cells {
             }
             this.cells[i] = tempArray;
         }
-        this.cells[12][10] = 1;
-        this.cells[10][11] = 1;
-        this.cells[12][11] = 1;
-        this.cells[11][12] = 1;
-        this.cells[12][12] = 1;
+
+        for (var i = 2; i < this.rows; i+=25) {
+            for (var j = 2; j < this.columns; j+=20) {
+                let rand = Math.random() * 12;
+                if (rand < 3) {
+                    this.insertGlider1(i, j);
+                } else if (rand < 6 && rand > 3) {
+                    this.insertGlider2(i,j);
+                } else if(rand > 6 && rand < 9) {
+                    this.insertGlider3(i, j);
+                } else {
+                    this.insertGlider4(i, j);
+                }
+            }
+        }
+    }
+
+    insertGlider1(i, j) {
+        this.cells[i+2][j] = 1;
+        this.cells[i][j+1] = 1;
+        this.cells[i+2][j+1] = 1;
+        this.cells[i+1][j+2] = 1;
+        this.cells[i+2][j+2] = 1;
+    }
+
+    insertGlider2(i, j) {
+        this.cells[i][j+1] = 1;
+        this.cells[i+1][j] = 1;
+        this.cells[i+2][j] = 1;
+        this.cells[i+2][j+1] = 1;
+        this.cells[i+2][j+2] = 1;
+    }
+
+    insertGlider3(i, j) {
+        this.cells[i][j] = 1;
+        this.cells[i][j+1] = 1;
+        this.cells[i+1][j] = 1;
+        this.cells[i+1][j+2] = 1;
+        this.cells[i+2][j] = 1;
+    }
+
+    insertGlider4(i, j) {
+        this.cells[i][j] = 1;
+        this.cells[i][j+1] = 1;
+        this.cells[i][j+2] = 1;
+        this.cells[i+1][j+2] = 1;
+        this.cells[i+2][j+1] = 1;
+    }
+
+    initializeWithGosper() {
+        for (var i = 0; i < this.rows; i++) {
+            let tempArray = new Array(this.columns);
+            for (var j = 0; j < this.columns; j++) {
+                tempArray[j] = 0;
+            }
+            this.cells[i] = tempArray;
+        }
+        let x=5;
+        let y = 5;
+        this.cells[y+6][x+2]=1;
+        this.cells[y+7][x+2]=1;
+        this.cells[y+6][x+3]=1;
+        this.cells[y+7][x+3]=1;
+        this.cells[y+6][x+12]=1;
+        this.cells[y+7][x+12]=1;
+        this.cells[y+8][x+12]=1;
+        this.cells[y+5][x+13]=1;
+        this.cells[y+9][x+13]=1;
+        this.cells[y+4][x+14]=1;
+        this.cells[y+10][x+14]=1;
+        this.cells[y+4][x+15]=1;
+        this.cells[y+10][x+15]=1;
+        this.cells[y+7][x+16]=1;
+        this.cells[y+5][x+17]=1;
+        this.cells[y+9][x+17]=1;
+        this.cells[y+6][x+18]=1;
+        this.cells[y+7][x+18]=1;
+        this.cells[y+8][x+18]=1;
+        this.cells[y+7][x+19]=1;
+        this.cells[y+4][x+22]=1;
+        this.cells[y+5][x+22]=1
+        this.cells[y+6][x+22]=1;
+        this.cells[y+4][x+23]=1;
+        this.cells[y+5][x+23]=1
+        this.cells[y+6][x+23]=1;
+        this.cells[y+3][x+24]=1;
+        this.cells[y+7][x+24]=1;
+        this.cells[y+2][x+26]=1;
+        this.cells[y+3][x+26]=1;
+        this.cells[y+7][x+26]=1;
+        this.cells[y+8][x+26]=1;
+        this.cells[y+4][x+36]=1;
+        this.cells[y+5][x+36]=1;
+        this.cells[y+4][x+37]=1;
+        this.cells[y+5][x+37]=1;
+
     }
 
     update() {
--- main.js
+++ main.js
@@ -4,6 +4,21 @@
 var canvas = document.getElementById("gameWorld");
 var ctx = canvas.getContext("2d");
 
+var gliderButton = document.getElementById("gliderState");
+gliderButton.addEventListener("click", function(){
+    cells.initializeWithGlider();
+});
+
+var randomButton = document.getElementById("randomState");
+randomButton.addEventListener("click", function() {
+    cells.initialize();
+});
+
+var gosperButton = document.getElementById("gosperState");
+gosperButton.addEventListener("click", function() {
+   cells.initializeWithGosper(); 
+});
+
 var cells = new Cells(280, 280, 2.5, ctx);
 cells.initialize();
 cells.draw();
@@ -12,8 +27,14 @@ var lastUpdate = Date.now();
 var currentTime;
 var timeStep = 100;
 
-var loop = window.setInterval(() => {
-    ctx.clearRect(0, 0, 800, 800);
-    cells.update();
-    cells.draw();
-}, timeStep);
+var loop;
+
+function setupTimer(timeStep) {
+    loop = window.setInterval(() => {
+        ctx.clearRect(0, 0, 800, 800);
+        cells.update();
+        cells.draw();
+    }, timeStep);
+}
+
+setupTimer(timeStep);
